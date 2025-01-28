2989e16836b4eb49db4ffddcf8baa3fc7649a201
belzevol <belzevol@gmail.com>
2025-01-28 17:17:44 +0200

    fin

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,52 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./contactsOps";
-import customToast from "../components/Toast/Toast";
-
-const handlePending = (state) => {
-  state.isLoading = true;
-};
-
-const handleRejected = (state, action) => {
-  state.isLoading = false;
-  state.error = action.payload;
-  customToast("error", "Oops... something went wrong, try again!");
-};
-
-const handleFulfilled = (state) => {
-  state.isLoading = false;
-  state.error = null;
-};
 
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: {
     items: [],
-    isLoading: false,
+    loading: false,
     error: null,
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.loading = false;
         state.items = action.payload;
-        customToast("success", "Countacts are loaded");
       })
-      .addCase(fetchContacts.rejected, handleRejected)
-      .addCase(addContact.pending, handlePending)
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(addContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
       .addCase(addContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.loading = false;
         state.items.push(action.payload);
-        customToast("success", `Contact added: ${action.payload.name}`);
       })
-      .addCase(addContact.rejected, handleRejected)
-      .addCase(deleteContact.pending, handlePending)
+      .addCase(addContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(deleteContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
       .addCase(deleteContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
-        state.items = state.items.filter((item) => item.id !== action.payload);
-        customToast("success", "Contact removed");
+        state.loading = false;
+        state.items = state.items.filter(
+          (contact) => contact.id !== action.payload
+        );
       })
-      .addCase(deleteContact.rejected, handleRejected);
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
 
